8198a5364fcce1d52630d3851b3ee368ccce330a
gmllanes-IT <gerald@mutant.ae>
2024-01-11 21:42:12 +0800

    Service Table Migration Update

--- database/migrations/2024_01_05_074719_create_services_table.php
+++ database/migrations/2024_01_05_074719_create_services_table.php
@@ -14,14 +14,14 @@ return new class extends Migration
         Schema::create('services', function (Blueprint $table) {
             $table->id();
             $table->string('en_name')->nullable();
-            $table->string('en_intro')->nullable();
-            $table->string('en_description', 2000)->nullable();
+            $table->text('en_intro')->nullable();
+            $table->text('en_description')->nullable();
             $table->string('image')->nullable();
-            $table->string('en_stepsprocedure', 5000)->nullable();
+            $table->text('en_stepsprocedure')->nullable();
             $table->string('ar_name')->nullable();
             $table->string('ar_intro')->nullable();
-            $table->string('ar_description', 2000)->nullable();
-            $table->string('ar_stepsprocedure', 5000)->nullable();
+            $table->text('ar_description')->nullable();
+            $table->text('ar_stepsprocedure')->nullable();
             $table->unsignedBigInteger('category_id')->nullable();
             $table->foreign('category_id')->references('id')->on('service_categories')->cascadeOnDelete();
             $table->timestamps();
